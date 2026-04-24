2e37e2b764cad0691985170a000ee3f84eb492e7
Wesley69KX <84612535+Wesley69KX@users.noreply.github.com>
2026-04-24 02:19:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ const app = {
 
         if (nomeDigitado.length < 3) return alert("Digite seu nome completo.");
         
-        const USUARIOS_PERMITIDOS = ["WESLEY", "CLEIDIVALDO" , "HEBER"]; 
+        const USUARIOS_PERMITIDOS = ["WESLEY", "CLEIDIVALDO" , "TESTE"]; 
         const SENHA_CORRETA = "1234";
 
         let usuarioExiste = false;
